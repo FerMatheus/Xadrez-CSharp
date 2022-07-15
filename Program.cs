110ec87a43faccf01442a52f6cbc310a1f1cff3e
Matheus Freitas <freitas.matheus.profissional@outlook.com>
2022-07-15 13:22:47 -0300

    Inicio do projeto Xadrez. Desenvolvendo um xadrez em C#.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using tabuleiro;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Posicao P;
+
+        P = new Posicao(3, 4);
+        Console.Write($"Posição: {P}");
+
+    }
+}
