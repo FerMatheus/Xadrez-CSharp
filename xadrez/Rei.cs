c7ae2ce5e414e5c6116219bf362a7020a61ed590
Matheus Freitas <freitas.matheus.profissional@outlook.com>
2022-07-22 11:24:00 -0300

    Adicionado a condição para executar o Roque curto e longo.

--- xadrez/Rei.cs
+++ xadrez/Rei.cs
@@ -4,13 +4,21 @@ namespace xadrez
 {
     public class Rei : Peca
     {
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        private PartidaDeXadrez partida;
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
         {
+            this.partida = partida;
         }
         private bool PodeMover(Posicao pos)
         {
             Peca peca = Tab.Peca(pos);
             return peca == null || peca.Cor != Cor;
+        }
+
+        public bool TesteTorreRoque(Posicao pos)
+        {
+            Peca torre = Tab.Peca(pos);
+            return torre != null && torre is Torre && torre.Cor == Cor && torre.qtdeMovimentos == 0;
         }
         public override bool[,] MovimentosPossiveis()
         {
@@ -62,6 +70,33 @@ namespace xadrez
             if (Tab.VerificaPosicao(pos) && PodeMover(pos))
             {
                 movimentosPossiveis[pos.Linha, pos.Coluna] = true;
+            }
+            // #JogadaEspecial = Roque
+            if (qtdeMovimentos == 0 && !partida.Xeque)
+            {
+                // Roque curto
+                Posicao posTorre1 = new(Posicao.Linha, Posicao.Coluna + 3);
+                if (TesteTorreRoque(posTorre1))
+                {
+                    Posicao p1 = new(Posicao.Linha, Posicao.Coluna + 1);
+                    Posicao p2 = new(Posicao.Linha, Posicao.Coluna + 2);
+                    if (Tab.Peca(p1) == null && Tab.Peca(p2) == null)
+                    {
+                        movimentosPossiveis[Posicao.Linha, Posicao.Coluna + 2] = true;
+                    }
+                }
+                //Rque Longo
+                Posicao posTorre2 = new(Posicao.Linha, Posicao.Coluna - 4);
+                if (TesteTorreRoque(posTorre2))
+                {
+                    Posicao p1 = new(Posicao.Linha, Posicao.Coluna - 1);
+                    Posicao p2 = new(Posicao.Linha, Posicao.Coluna - 2);
+                    Posicao p3 = new(Posicao.Linha, Posicao.Coluna - 3);
+                    if (Tab.Peca(p1) == null && Tab.Peca(p2) == null && Tab.Peca(p3) == null)
+                    {
+                        movimentosPossiveis[Posicao.Linha, Posicao.Coluna - 2] = true;
+                    }
+                }
             }
             return movimentosPossiveis;
         }
